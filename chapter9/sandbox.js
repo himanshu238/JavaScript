2a9aefb424657acce50d2b10842c2bf69188e90f
himanshuS <hs469908@gmail.com>
2023-11-14 17:25:33 +0530

    type conversion added

---
+++ chapter9/sandbox.js
@@ -0,0 +1,20 @@
+//Type conversion
+let num = '100';
+num += 1;
+console.log('Num: ', num);
+
+let newNum = 100;
+newNum = Number(newNum);
+newNum += 1;
+console.log('Num: ', newNum);
+
+console.log(typeof newNum)
+
+let result = String(550);
+console.log(result, typeof result);
+
+let isDone = Boolean(100);
+console.log(isDone, typeof isDone);
+
+
+
